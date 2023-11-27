3b89e1fb15d3b66dcdbada96a290f1b455673fff
Ulisses Nascimento <ulisses.nascimento@crednovo.com.br>
2023-11-27 19:43:39 -0300

    adicionando correlação

--- main.py
+++ main.py
@@ -18,16 +18,27 @@ def main():
     df_quantidade_di_de['mês de desembaraço'] = df_quantidade_di_de['mês de desembaraço'].apply(lambda ano: pd.to_numeric(str(ano)[4:6]))
     df_quantidade_di_de['trimestre de desembaraço'] = df_quantidade_di_de['mês de desembaraço'].apply(lambda mes: (mes - 1) // 3 + 1 if 1 <= mes <= 12 else None)
     df_quantidade_di_de = pd.DataFrame(df_quantidade_di_de.groupby(['código ua local de despacho', 'ano de desembaraço', 'trimestre de desembaraço']).sum())
+    df_quantidade_di_de = df_quantidade_di_de.reset_index()
+
     #Tratando o dataframe de importacoes
     df_importacoes_por_regiao['TRIMESTRE DESEMB'] = df_importacoes_por_regiao['TRIMESTRE DESEMB'].apply(lambda trimestre: str(trimestre)[4])
 
+    #Convertendo os tipos de dados para string
+    df_importacoes_por_regiao['TRIMESTRE DESEMB'] = df_importacoes_por_regiao['TRIMESTRE DESEMB'].astype(str)
+    df_importacoes_por_regiao['ANO DESEMB'] = df_importacoes_por_regiao['ANO DESEMB'].astype(str)
+    df_quantidade_di_de['trimestre de desembaraço'] = df_quantidade_di_de['trimestre de desembaraço'].astype(str)
 
     print(df_importacoes_por_regiao.head(5))
     print(df_quantidade_di_de.head(10))
 
-    report = sv.analyze(df_quantidade_di_de)
-    report = sv.analyze(df_importacoes_por_regiao)
+    df_correlacionado = pd.merge(df_importacoes_por_regiao, df_quantidade_di_de, how='inner', left_on=['UA LOCAL DESPACHO', 'ANO DESEMB', 'TRIMESTRE DESEMB'], right_on=['código ua local de despacho', 'ano de desembaraço', 'trimestre de desembaraço'])
+
+    df_correlacionado = df_correlacionado.reset_index()
+
+    report = sv.analyze(df_correlacionado)
     report.show_html()
+
+    print(df_correlacionado.loc[100])
 
 
 if __name__ == "__main__":
